eda8963e7325d023cb4ee1ed211ae34186d345c1
jgargar558j <jgargar558j@g.educaand.es>
2024-01-26 21:50:40 +0100

    2 Commit. 0.2 Version of the App. Added: - Next and previous buttons pokemon in the view of the pokemon - Button to go back to the main view - Changed font on description of pokemon in main view
    For the next commit i will try to upload the navigation search

--- app/src/main/java/es/iessaladillo/jgargar558j/pokemonwiki/Pokemon.kt
+++ app/src/main/java/es/iessaladillo/jgargar558j/pokemonwiki/Pokemon.kt
@@ -2,7 +2,7 @@ package es.iessaladillo.jgargar558j.pokemonwiki
 
 import java.io.Serializable
 
-class Pokemon(_id: Int, _name: String, _description: String, _imagen:Int, _tipo1:Int, _tipo2:Int, _imagenVariocolor:Int) : Serializable{
+class Pokemon(_id: Int, _name: String, _description: String, _imagen:Int, _tipo1:Int, _tipo2:Int, _imagenVariocolor:Int, _idString:String) : Serializable{
     var id:Int = _id
     var name:String = _name
     var description:String = _description
@@ -10,4 +10,5 @@ class Pokemon(_id: Int, _name: String, _description: String, _imagen:Int, _tipo1
     var tipo1:Int = _tipo1
     var tipo2:Int = _tipo2
     var imagenVariocolor:Int = _imagenVariocolor
+    var idString:String = _idString
 }
--- app/src/main/java/es/iessaladillo/jgargar558j/pokemonwiki/PokemonActivity.kt
+++ app/src/main/java/es/iessaladillo/jgargar558j/pokemonwiki/PokemonActivity.kt
@@ -1,11 +1,14 @@
 package es.iessaladillo.jgargar558j.pokemonwiki
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Switch
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PokemonActivity : AppCompatActivity() {
     lateinit var tvNombre:TextView
@@ -15,14 +18,33 @@ class PokemonActivity : AppCompatActivity() {
     lateinit var ivTipo2:ImageView
     lateinit var swVarioColor:Switch
     lateinit var tvNumero:TextView
+    lateinit var ibAnterior:ImageButton
+    lateinit var ibSiguiente:ImageButton
+    lateinit var pokemon: Pokemon
+    lateinit var fabVolver:FloatingActionButton
+    var next: Pokemon? = null
+    var previous: Pokemon? = null
+    lateinit var pokedex:ArrayList<Pokemon>
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon)
 
         initializeVariables()
-        val pokemon = intent.getSerializableExtra("pokemon") as Pokemon
+        var bundle = Bundle()
+        bundle = intent.getBundleExtra("bundlelist")!!
+        pokedex = bundle.getSerializable("fPokemon") as ArrayList<Pokemon>
+        pokemon = intent.getSerializableExtra("pokemon") as Pokemon
+        if(intent.getSerializableExtra("previous")==null){
+            next = intent.getSerializableExtra("next") as Pokemon
+        }else if(intent.getSerializableExtra("next")==null){
+            previous = intent.getSerializableExtra("previous") as Pokemon
+        }else{
+            previous = intent.getSerializableExtra("previous") as Pokemon
+            next = intent.getSerializableExtra("next") as Pokemon
+        }
+
         tvNombre.text = pokemon.name
         tvDescripcion.text = "Descripción: "+pokemon.description
         ivPokemon.setImageResource(pokemon.imagen)
@@ -36,13 +58,84 @@ class PokemonActivity : AppCompatActivity() {
                 ivPokemon.setImageResource(pokemon.imagen)
             }
         }
-
         when(getString(pokemon.id).toInt()){
             in 0..9 -> {tvNumero.text = "#00"+getString(pokemon.id)}
             in 10..99 -> {tvNumero.text = "#0"+getString(pokemon.id)}
             in 100..151 -> {tvNumero.text = "#"+getString(pokemon.id)}
         }
+        if ((getString(pokemon.id).toInt())==1){
+            ibAnterior.setImageResource(R.drawable.vacio)
+            ibAnterior.setOnClickListener { }
+            if (next != null) {
+                ibSiguiente.setImageResource(next!!.imagen)
+            }
+            ibSiguiente.setOnClickListener {
+                next()
+            }
+        }else if ((getString(pokemon.id).toInt())==(getString(R.integer.totalNumberOfPokemonInPokedex).toInt())){
+            ibSiguiente.setImageResource(R.drawable.vacio)
+            ibSiguiente.setOnClickListener { }
+            if (previous != null) {
+                ibAnterior.setImageResource(previous!!.imagen)
+            }
+            ibAnterior.setOnClickListener {
+                previous()
+            }
+        }else{
+            if (next != null) {
+                ibSiguiente.setImageResource(next!!.imagen)
+            }
+            if (previous != null) {
+                ibAnterior.setImageResource(previous!!.imagen)
+            }
+            ibAnterior.setOnClickListener {
+                previous()
+            }
+            ibSiguiente.setOnClickListener {
+                next()
+            }
+        }
+
+        fabVolver.setOnClickListener{
+            startActivity(Intent(this,Principal_Activity::class.java))
+        }
     }
+
+    @SuppressLint("ResourceType")
+    fun next(){
+        val intent = Intent(this,PokemonActivity::class.java)
+        var bundle = Bundle()
+        var bundleList:ArrayList<Pokemon> = ArrayList(pokedex)
+        bundle.putSerializable("fPokemon",bundleList)
+        intent.putExtra("bundlelist",bundle)
+        if(pokedex[next!!.idString.toInt()-1].id==getString(R.integer.totalNumberOfPokemonInPokedex).toInt()){
+            intent.putExtra("pokemon",next)
+            intent.putExtra("previous",pokemon)
+        }else{
+            intent.putExtra("pokemon",next)
+            intent.putExtra("previous",pokemon)
+            intent.putExtra("next",pokedex[next!!.idString.toInt()])
+        }
+        startActivity(intent)
+    }
+    @SuppressLint("ResourceType")
+    fun previous(){
+        val intent = Intent(this,PokemonActivity::class.java)
+        var bundle = Bundle()
+        var bundleList:ArrayList<Pokemon> = ArrayList(pokedex)
+        bundle.putSerializable("fPokemon",bundleList)
+        intent.putExtra("bundlelist",bundle)
+        if (pokedex[previous!!.idString.toInt()-1].id==0){
+            intent.putExtra("pokemon",previous)
+            intent.putExtra("next",pokemon)
+        }else{
+            intent.putExtra("pokemon",previous)
+            intent.putExtra("next",pokemon)
+            intent.putExtra("previous",pokedex[previous!!.idString.toInt()-2])
+        }
+        startActivity(intent)
+    }
+
     fun initializeVariables(){
         tvNombre=findViewById(R.id.TVnombreD)
         ivPokemon=findViewById(R.id.IVpokemonD)
@@ -51,5 +144,8 @@ class PokemonActivity : AppCompatActivity() {
         ivTipo2=findViewById(R.id.IVtipo2)
         swVarioColor=findViewById(R.id.SWVarioColor)
         tvNumero=findViewById(R.id.TVnumero)
+        ibAnterior=findViewById(R.id.IBAnterior)
+        ibSiguiente=findViewById(R.id.IBSiguiente)
+        fabVolver=findViewById(R.id.FABVolver)
     }
 }
